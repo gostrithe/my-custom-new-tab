c1e8d2b5b3d63cc1425308f7c6a464a97bd48af8
jack <104426438+gostrithe@users.noreply.github.com>
2023-03-27 01:35:31 +0800

    source

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/"></RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/typedJs.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="background: red">
+    <h2>SDFADS DS SA D</h2>
+    <vue-typed-js :strings="['First text', 'Second Text']">
+      <h1 class="typing"></h1>
+    </vue-typed-js>
+    <!-- <Typed :options="options">
+      <h1 class="typing"></h1>
+    </Typed> -->
+  </div>
+</template>
+
+<script setup>
+// import VueTypedJs from 'vue-typed-js'
+// import Typed from 'vue3-typed-js'
+// const options = {
+//   strings: ['Hello', 'World', 'This is vue3-typed-js'],
+//   loop: true,
+//   typeSpeed: 30
+// }
+</script>

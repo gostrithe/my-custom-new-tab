304506bd2014f3b3e13b24cbd3ff78b0e65956d6
jack <104426438+gostrithe@users.noreply.github.com>
2023-04-02 04:37:39 +0800

    add am

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,24 @@
 import { onMounted, ref } from 'vue'
 // import typedJS from '../components/typedJs.vue'
 const sixResult = ref('')
-onMounted(() => {
+const amResult = ref('')
+const isDisabled = ref(true)
+const fetchData = async () => {
+  let res = await fetch(`http://api.bjjfnet.com/data/opencode/2032`, {
+    method: 'GET',
+    mode: 'cors'
+  })
+  const json = await res.json()
+  console.log(json)
+  let flag = true
+  for (let key in json.data[0]) {
+    if (typeof json.data[0][key] !== 'string') {
+      flag = false
+    }
+  }
+  if (flag) {
+    amResult.value = json.data[0]
+  }
   fetch('https://1680660.com/smallSix/findSmallSixInfo.do', {
     method: 'GET',
     headers: {
@@ -17,9 +34,33 @@ onMounted(() => {
       return res.json()
     })
     .then((res) => {
-      sixResult.value = res.result.data
+      console.log(res)
+      let a = res.result.data
+      if (
+        typeof a.preDrawCode == 'string' &&
+        typeof a.preDrawIssue == 'number' &&
+        typeof a.preDrawTime == 'string'
+      ) {
+        sixResult.value = res.result.data
+      }
     })
+}
+onMounted(async () => {
+  fetchData()
 })
+let count = 0
+let timer = null
+const openInput = () => {
+  clearTimeout(timer)
+  count++
+  if (count === 3) {
+    isDisabled.value = false
+    return
+  }
+  timer = setTimeout(() => {
+    count = 0
+  }, 500)
+}
 
 const goToSearch = (e) => {
   window.open(`https://cn.bing.com/search?q=${e.target.value}`, '_blank')
@@ -31,13 +72,28 @@ const goToSearch = (e) => {
     <div class="circleAnimate"></div>
     <div id="element"></div>
     <!-- <typedJS></typedJS> -->
-    <div class="search">
-      <input placeholder="晚上好, 妈妈" @keyup.enter="goToSearch($event)" />
+    <div class="search" @click="openInput">
+      <input :disabled="isDisabled" placeholder="晚上好, 妈妈" @keyup.enter="goToSearch($event)" />
     </div>
-    <div style="padding-top: 50vh; font-size: 20px; color: red">
+    <div style="padding-top: 35vh; font-size: 20px; color: red">
+      <div
+        class="freshBtn"
+        @click="fetchData"
+        style="
+          margin: 0 auto;
+          line-height: 2;
+          border-radius: 2rem;
+          width: 50vw;
+          text-align: center;
+          background: mediumturquoise;
+          color: #111;
+        "
+      >
+        刷新一下
+      </div>
       <p>{{ sixResult.preDrawTime }}</p>
-      <p>{{ sixResult.preDrawIssue }}</p>
-      <p style="background: darkslateblue; padding: 12px; font-size: 30px">
+      <span>{{ sixResult.preDrawIssue }} -- 香港</span>
+      <div style="background: darkslateblue; padding: 12px; font-size: 30px">
         <span
           class="ball"
           :style="index == 6 ? 'margin-left: 15px;color:blue' : ''"
@@ -46,12 +102,36 @@ const goToSearch = (e) => {
         >
           {{ item }}
         </span>
-      </p>
+      </div>
+    </div>
+    <hr />
+    <div style="font-size: 20px; color: red">
+      <p>{{ amResult.openTime }}</p>
+      <span>{{ amResult.issue }} -- 澳门</span>
+      <div style="background: darkslateblue; padding: 12px; font-size: 30px">
+        <span
+          class="ball"
+          :style="index == 6 ? 'margin-left: 15px;color:blue' : ''"
+          v-for="(item, index) in amResult.openCode?.split(',')"
+          :key="index"
+        >
+          {{ item }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.freshBtn {
+  transition: all 0.5s;
+}
+.freshBtn:active {
+  color: #fff !important;
+  background: rgb(130, 204, 201) !important;
+  transform: scale(1.2);
+}
+
 .ball {
   width: 45px;
   height: 45px;
